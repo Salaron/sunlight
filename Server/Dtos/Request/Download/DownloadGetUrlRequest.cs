2febd59e9b5e99c41d0abccb97b5104c45bc9fd1
Salaron <27885055+Salaron@users.noreply.github.com>
2023-04-09 19:40:18 +0500

    stub download/getUrl

---
+++ Server/Dtos/Request/Download/DownloadGetUrlRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SunLight.Dtos.Request.Download;
+
+[Serializable]
+public class DownloadGetUrlRequest : ClientRequest
+{
+    public string Os { get; init; }
+    public IEnumerable<string> PathList { get; init; }
+}
