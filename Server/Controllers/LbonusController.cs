ef0fbbf3c061962020dc926722459f3fc1d23397
Salaron <yo.salaron@gmail.com>
2023-04-09 19:39:07 +0500

    Stub lbonus controller

---
+++ Server/Controllers/LbonusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SunLight.Authorization;
+using SunLight.Dtos.Request;
+using SunLight.Dtos.Response;
+using SunLight.Dtos.Response.Award;
+
+namespace SunLight.Controllers;
+
+[Authorize]
+[ApiController]
+[XMessageCodeCheck(specialApi: true)]
+[Route("main.php/lbonus")]
+public class LbonusController : LlsifController
+{
+
+    [HttpPost("execute")]
+    [Produces(typeof(ServerResponse<AwardInfoResponse>))]
+    public async Task<IActionResult> Execute([FromBody] ClientRequest requestData)
+    {
+        var response = Enumerable.Empty<EmptyResponse>();
+        return SendResponse(response);
+    }
+}
