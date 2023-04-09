de5279de48c66be96bcd2da76356f9b8ee71b872
Salaron <27885055+Salaron@users.noreply.github.com>
2023-04-09 19:38:39 +0500

    Log not implemented api's

--- Server/Middlewares/MiddlewareExtensions.cs
+++ Server/Middlewares/MiddlewareExtensions.cs
@@ -2,13 +2,13 @@
 
 internal static class MiddlewareExtensions
 {
-    public static IApplicationBuilder UsePerformanceMeter(this IApplicationBuilder builder)
-    {
-        return builder.UseMiddleware<PerformanceMeterMiddleware>();
-    }
-
     public static IApplicationBuilder UseCustomResponseHeaders(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<CustomResponseHeadersMiddleware>();
+    }
+    
+    public static IApplicationBuilder UseNotFoundLogger(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<NotFoundLoggerMiddleware>();
     }
 }
---
+++ Server/Middlewares/NotFoundLoggerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace SunLight.Middlewares;
+
+internal class NotFoundLoggerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<NotFoundLoggerMiddleware> _logger;
+
+    public NotFoundLoggerMiddleware(RequestDelegate next, ILogger<NotFoundLoggerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        await _next(httpContext);
+
+        if (httpContext.Response.StatusCode == (int)HttpStatusCode.NotFound)
+        {
+            httpContext.Request.Body.Seek(0, SeekOrigin.Begin);
+            using var requestBodyStreamReader = new StreamReader(httpContext.Request.Body);
+            var requestBody = await requestBodyStreamReader.ReadToEndAsync();
+            _logger.LogWarning(
+                $"Handler for {httpContext.Request.Path} is not implemented" +
+                Environment.NewLine +
+                $"Request body was {requestBody}");
+        }
+    }
+}
