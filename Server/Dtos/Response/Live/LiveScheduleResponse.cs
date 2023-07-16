ba66f20d828dd0e0c2c75483da3a93d306df1784
Salaron <yo.salaron@gmail.com>
2023-07-17 01:44:09 +0500

    add support for special lives

--- Server/Dtos/Response/Live/LiveScheduleResponse.cs
+++ Server/Dtos/Response/Live/LiveScheduleResponse.cs
@@ -3,8 +3,16 @@
 [Serializable]
 public class LiveScheduleResponse
 {
+    public record LiveScheduleInfo
+    {
+        public int LiveDifficultyId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsRandom { get; set; }
+    }
+
     public IEnumerable<object> EventList { get; set; }
-    public IEnumerable<object> LiveList { get; set; }
+    public IEnumerable<LiveScheduleInfo> LiveList { get; set; }
     public IEnumerable<object> LimitedBonusList { get; set; }
     public IEnumerable<object> LimitedBonusCommonList { get; set; }
     public IEnumerable<object> RandomLiveList { get; set; }
